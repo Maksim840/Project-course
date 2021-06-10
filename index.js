254e90c3ea2b23e457192fa6efdc63d8f4687866
Maks <maksimmiksha@mail.ru>
2021-06-10 12:16:28 +0300

    First Project commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+let nunberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = { 
+    count: nunberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?'),
+      b = prompt('На сколько оцените его?'),
+      c = prompt('Один из последних просмотренных фильмов?'),
+      d = prompt('На сколько оцените его?');    
+    
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = b;    
+console.log(personalMovieDB);
